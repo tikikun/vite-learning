b153b27acc4696a29011ef7835f4d3d512abdbfe
Dao Tuan <daotuan@Daos-MacBook-Pro.local>
2021-09-05 10:57:35 +0700

    first commit

---
+++ src/components/ReviewForm.vue
@@ -0,0 +1,66 @@
+<template>
+  <form class="review-form" @submit.prevent="onSubmit">
+    <h3>Leave a review</h3>
+    <label for="name">Name:</label>
+    <input id="name" v-model="name" />
+
+    <label for="review">Review:</label>
+    <textarea id="review" v-model="review"></textarea>
+
+    <label for="rating">Rating:</label>
+    <select id="rating" v-model.number="rating">
+      <option>5</option>
+      <option>4</option>
+      <option>3</option>
+      <option>2</option>
+      <option>1</option>
+    </select>
+
+    <label for="recommend">Recommend:</label>
+    <select id="recommend" v-model="recommend">
+      <option>Yes</option>
+      <option>No</option>
+    </select>
+
+    <input class="button" type="submit" value="Submit" />
+  </form>
+</template>
+
+<script>
+export default {
+  setup() {},
+  data() {
+    return {
+      name: "",
+      review: "",
+      rating: null,
+      recommend: null,
+    };
+  },
+  methods: {
+    onSubmit() {
+      if (
+        this.name == "" ||
+        this.review == "" ||
+        this.rating == null ||
+        this.recommend == null
+      ) {
+        alert("Review is incomplete please fill out everything ");
+        return;
+      }
+      let productReview = {
+        name: this.name,
+        review: this.review,
+        rating: this.rating,
+        recommend: this.recommend,
+      };
+      this.$emit("reviewed-submitted", productReview);
+
+      this.name = "";
+      this.review = "";
+      this.rating = null;
+      this.recommend = null;
+    },
+  },
+};
+</script>
